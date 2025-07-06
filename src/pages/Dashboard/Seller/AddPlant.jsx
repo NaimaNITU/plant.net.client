13b88446ea9ff8513ad2c76fc1a2910dda215596
Robiul Hasan Raian <toktikproject@gmail.com>
2025-07-06 17:59:46 +0600

    first commit

---
+++ src/pages/Dashboard/Seller/AddPlant.jsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+import AddPlantForm from "../../../components/Form/AddPlantForm";
+
+const AddPlant = () => {
+  const handleAddPlantForm = async (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const category = form.category.value;
+    const description = form.description.value;
+    const price = form.price.value;
+    const quantity = form.quantity.value;
+    const image = form.image.files;
+    const plantData = { category, description, price, quantity, image };
+    console.log(plantData);
+
+    const { data } = await axios.post(
+      `https://api.imgbb.com/1/upload?key=${
+        import.meta.env.VITE_IMGBB_API_KEY
+      }`,
+      image
+    );
+    console.log(data);
+  };
+  return (
+    <div>
+      {/* Form */}
+      <AddPlantForm handleAddPlantForm={handleAddPlantForm} />
+    </div>
+  );
+};
+
+export default AddPlant;
---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,101 @@
+/* eslint-disable react/prop-types */
+import { createContext, useEffect, useState } from "react";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import { app } from "../firebase/firebase.config";
+import axios from "axios";
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  //creating/sign up/register user through email
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  //sign in/login user through email
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  //sign in/login user through google
+  const signInWithGoogle = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  //logout user
+  const logOut = async () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  const updateUserProfile = (name, photo) => {
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    });
+  };
+
+  // onAuthStateChange
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+      console.log("CurrentUser-->", currentUser?.email);
+      if (currentUser?.email) {
+        setUser(currentUser);
+
+        // Get JWT token
+        await axios.post(
+          `${import.meta.env.VITE_API_URL}/jwt`,
+          {
+            email: currentUser?.email,
+          },
+          { withCredentials: true }
+        );
+      } else {
+        setUser(currentUser);
+        await axios.get(`${import.meta.env.VITE_API_URL}/logout`, {
+          withCredentials: true,
+        });
+      }
+      setLoading(false);
+    });
+    return () => {
+      return unsubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    user,
+    setUser,
+    loading,
+    setLoading,
+    createUser,
+    signIn,
+    signInWithGoogle,
+    logOut,
+    updateUserProfile,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
